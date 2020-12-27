b946c05946c4d22521eeb15fa420898e42d4fa82
Randy Shao <17rs63@queensu.ca>
2020-12-26 19:31:38 -0500

    Additional nomination disabling logic

--- src/components/SearchList/SearchResult.js
+++ src/components/SearchList/SearchResult.js
@@ -1,10 +1,24 @@
-import { useState, useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { NominationsContext } from '../../context/NominationsContext';
 import styles from './SearchList.module.css';
 
 const SearchResult = ({ movie }) => {
   const { nominations, addNomination } = useContext(NominationsContext);
   const [disabled, setDisabled] = useState(false);
+
+  useEffect(() => {
+    if (nominations.length === 0) {
+      setDisabled(false);
+    }
+    for (let i = 0; i < nominations.length; i++) {
+      if (movie.imdbID === nominations[i].imdbID) {
+        setDisabled(true);
+        break;
+      } else {
+        setDisabled(false);
+      }
+    }
+  }, [nominations]);
 
   const nominationSubmit = (movie) => {
     if (nominations.length <= 4) {
